41dcf34545564de9c30cddfe4a093a945aa13d03
sundaycrave <34419250+sundaycrave@users.noreply.github.com>
2021-05-31 03:05:21 +0530

    Update config.lua

--- animatedLivery/config.lua
+++ animatedLivery/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
 Config.cars = {
-    {["Model"] = "", ["Texture"] = "", ["Image"] = ""},
-    {["Model"] = "", ["Texture"] = "", ["Image"] = ""}
+    {["Model"] = "audipd", ["Texture"] = "r8lms_sign_4", ["Image"] = "https://media.giphy.com/media/26uf40zlwUk2FU95m/giphy.gif"},
+    -- Template {["Model"] = "", ["Texture"] = "", ["Image"] = ""}
 }
